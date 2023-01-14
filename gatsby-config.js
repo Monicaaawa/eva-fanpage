03e01ceae517e0fd0a753a5e4a6b447b50c41e51
Shiyu Ye <shiyuye@Shiyus-MacBook-Pro.local>
2023-01-14 14:46:44 -0800

    Configure React app for deployment to GitHub Pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,7 @@ module.exports = {
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    pathPrefix: "/evangelion-fanpage",
   },
   plugins: [
     "gatsby-plugin-sass",
