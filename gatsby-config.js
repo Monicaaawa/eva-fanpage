3c002ec1f882b54b3ad5e422fccce686f1ddd86f
Shiyu Ye <shiyuye@Shiyus-MacBook-Pro.local>
2023-01-14 19:37:22 -0800

    Configure React app for deployment to GitHub Pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,12 +9,12 @@
  */
 module.exports = {
   siteMetadata: {
-    pathPrefix: "/eva-fanpage",
     title: `Neon Genesis Evangelion`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
+  pathPrefix: "/eva-fanpage",
   plugins: [
     "gatsby-plugin-sass",
     `gatsby-plugin-image`,
